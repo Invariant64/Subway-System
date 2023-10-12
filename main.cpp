a2bbfa9b9e1c9330b2c2eee231d12ccc7d44b111
Invariant64 <98544022+Invariant64@users.noreply.github.com>
2023-10-12 19:13:35 +0800

    修改Line存储格式

--- main.cpp
+++ main.cpp
@@ -21,14 +21,14 @@ void printPath(Net *net, const QList<int>& path) {
     for (int i = 0; i < path.size(); i++) {
         std::cout << net->getStationById(path[i]).getName().toStdString() << " ";
         if (i == 0) {
-            std::cout << "(" << net->getLineById(line_id).getName().toStdString() << ") -> ";
+            std::cout << "(" << net->getLineById(line_id)->getName().toStdString() << ") -> ";
             last_line_id = line_id;
         }
         else if (i != path.size() - 1) {
             line_id = net->getEdgeByStationId(path[i], path[i + 1]).getLineId();
             if (last_line_id != line_id && last_line_id != -1) {
-                std::cout << "(从 " << net->getLineById(last_line_id).getName().toStdString()
-                          << " 转乘至 " << net->getLineById(line_id).getName().toStdString() << ") ";
+                std::cout << "(从 " << net->getLineById(last_line_id)->getName().toStdString()
+                          << " 转乘至 " << net->getLineById(line_id)->getName().toStdString() << ") ";
                 last_line_id = line_id;
             }
             std::cout << "-> ";
@@ -37,26 +37,31 @@ void printPath(Net *net, const QList<int>& path) {
 }
 
 int main(int argc, char *argv[]) {
-//    QApplication app(argc, argv);
+    QApplication app(argc, argv);
+
+    MyWidget main_window;
+
+    Net *net = new Net(Station_File_Path, Edge_File_Path, Line_File_Path);
+    main_window.setNet(net);
+    main_window.initComboBoxLine();
+
+    main_window.show();
+
+//    Net net(Station_File_Path, Edge_File_Path, Line_File_Path);
 //
-//    MainWindow main_window;
-//    main_window.show();
+//    std::string from, to;
+//    std::cin >> from >> to;
+//
+//    QList<int> time_path;
+//    std::cout << "time cost is " << net.getShortestPath(
+//            net.getStationIdByName(QString::fromStdString(from)),
+//            net.getStationIdByName(QString::fromStdString(to)),
+//            time_path, 1) << std::endl;
+//
+//    printPath(&net, time_path);
 
-    Net net(Station_File_Path, Edge_File_Path, Line_File_Path);
-
-    std::string from, to;
-    std::cin >> from >> to;
-
-    QList<int> time_path;
-    std::cout << "time cost is " << net.getShortestPath(
-            net.getStationIdByName(QString::fromStdString(from)),
-            net.getStationIdByName(QString::fromStdString(to)),
-            time_path, 1) << std::endl;
-
-    printPath(&net, time_path);
-
-//    return QApplication::exec();
-    return 0;
+    return QApplication::exec();
+    // return 0;
 }
 
 
