d8edc88f86b6edcc3b2ea11a52e121608a6bd3c6
Invariant64 <98544022+Invariant64@users.noreply.github.com>
2023-11-13 20:23:45 +0800

    优化数据处理代码

--- subway-data/p.cpp
+++ subway-data/p.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <set>
 
 #define LINE_FILE "../line.txt"
 
@@ -10,23 +11,13 @@ using std::string;
 using std::cout;
 using std::endl;
 using std::map;
-
-// struct Edge {
-//     string from;
-//     string to;
-//     string line;
-//     int weight;
-// } edges[1000];
+using std::pair;
+using std::set;
 
 int edges_num = 0;
 int stations_num = 0;
 
 int main() {
-    // FILE *fp = fopen(LINE_FILE, "r");
-    // if (fp == NULL) {
-    //     std::cout << "Error: " << LINE_FILE << " not found" << std::endl;
-    //     return 1;
-    // }
 
     std::ifstream fp;
     std::ofstream of, of1;
@@ -45,6 +36,8 @@ int main() {
     }
     fp.close();
 
+    set< pair<int, int> > edges;
+
     // read edges and write edges and write lines
     of.open("../edges.txt", std::ios::out);
     if (!of.is_open()) {
@@ -56,7 +49,7 @@ int main() {
         std::cout << "Error: lines.txt not found" << std::endl;
         return 1;
     }
-    map<string, int> m;
+    map<string, int> station_to_id;
     for (int i = 0; i < num_lines; i++) {
         fp.open("../" + lines[i] + ".txt", std::ios::in);
         if (!fp.is_open()) {
@@ -65,35 +58,66 @@ int main() {
         }
         string from, to;
         int weight;
-        map<int, int> m1;
+        set<int> stations_in_line;
         while (fp >> from >> to >> weight) {
-            if (m.find(from) == m.end()) {
-                m[from] = stations_num++;
+            if (station_to_id.find(from) == station_to_id.end()) {
+                station_to_id[from] = stations_num++;
             }
-            if (m.find(to) == m.end()) {
-                m[to] = stations_num++;
+            if (station_to_id.find(to) == station_to_id.end()) {
+                station_to_id[to] = stations_num++;
             }
-            m1.insert(std::make_pair(m[from], 0));
-            m1.insert(std::make_pair(m[to], 0));
-            of << edges_num << ' ' << i << ' ' << m[from] << ' ' << m[to] << ' ' << 0 << ' ' << weight << "\n";
-            edges_num++;
-            of << edges_num<< ' ' << i << ' ' << m[to] << ' ' << m[from] << ' ' << 0 << ' ' << weight << "\n";
-            edges_num++;
+            stations_in_line.insert(station_to_id[from]);
+            stations_in_line.insert(station_to_id[to]);
+            if (edges.find(std::make_pair(station_to_id[from], station_to_id[to])) == edges.end()) {
+                edges.insert(std::make_pair(station_to_id[from], station_to_id[to]));
+                of << edges_num << ' ' << i << ' ' << station_to_id[from] << ' ' << station_to_id[to] << ' ' << 0 << ' ' << weight << "\n";
+                edges_num++;
+            }
+            if (edges.find(std::make_pair(station_to_id[to], station_to_id[from])) == edges.end()) {
+                edges.insert(std::make_pair(station_to_id[to], station_to_id[from]));
+                of << edges_num << ' ' << i << ' ' << station_to_id[to] << ' ' << station_to_id[from] << ' ' << 0 << ' ' << weight << "\n";
+                edges_num++;
+            }
         }
         fp.close();
-        of1 << i << ' ' << lines[i] << ' ' << m1.size() << "\n";
-        for (auto it = m1.begin(); it != m1.end(); it++) {
-            of1 << it->first << ' ';
+        of1 << i << ' ' << lines[i] << ' ' << stations_in_line.size() << "\n";
+        for (auto it = stations_in_line.begin(); it != stations_in_line.end(); it++) {
+            of1 << *it << ' ';
         }
         of1 << "\n";
     }
     of.close();
     of1.close();
 
+    map< string, pair<int, int> > station_positions;
+    fp.open("../xy.txt", std::ios::in);
+    if (!fp.is_open()) {
+        std::cout << "Error: xy.txt not found" << std::endl;
+        return 1;
+    }
+    while (!fp.eof()) {
+        string station;
+        int x, y;
+        fp >> station >> x >> y;
+        station_positions.insert(std::make_pair(station, std::make_pair(x, y)));
+    }
+
     // write stations
-    of.open("../stations.txt", std::ios::out);
-    for (auto it = m.begin(); it != m.end(); it++) {
-        of << it->second << ' ' << it->first << "\n";
+    of.open("../data.station", std::ios::out);
+
+    map<int, string> id_to_station;
+    for (auto it = station_to_id.begin(); it != station_to_id.end(); it++) {
+        id_to_station.insert(std::make_pair(it->second, it->first));
+    }
+    for (auto it = id_to_station.begin(); it != id_to_station.end(); it++) {
+        of << it->first << ' ' << it->second;
+        if (station_positions.find(it->second) != station_positions.end()) {
+            of << ' ' << station_positions[it->second].first << ' ' << station_positions[it->second].second;
+        }
+        else {
+            of << ' ' << 0 << ' ' << 0;
+        }
+        of << "\n";
     }
     of.close();
 
